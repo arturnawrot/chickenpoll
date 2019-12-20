0f8eed492fd7083dd48d4f06f4a8f66447dee7c2
Arturek1 <artur.programista2@gmail.com>
2019-12-19 22:26:16 -0500

    Fixes from last commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,31 @@
 
 @section('title', 'Create a new poll')
 @section('description', 'Real-time, instant, ad-free and simple')
+
+@section('head')
+<?php
+$title = "ChickenPoll.com | Real time polls";
+$description = "No registration required. Real-time, instant, ad-free and simple";
+$options = $poll->options->implode('content', ' - ');
+?>
+<meta property="og:title" content="{{ $title }}"/>
+<meta property="og:site_name" content="{{ $description }}" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="{{ url()->full() }}" />
+<meta property="og:description" content="{{ $description }}" />
+<meta property="og:image" content="{{ asset('images/web.png') }}" />
+<meta name="title" content="{{ $title }}" />
+<meta name="type" content="website" />
+<meta name="url" content="{{ url()->full() }}" />
+<meta name="description" content="{{ $description }}" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@PollChicken" />
+<meta name="twitter:creator" content="@PollChicken" />
+<meta name="twitter:title" content="{{ $title }}" />
+<meta name="twitter:description" content="{{ $description }}" />
+<meta name="twitter:image" content="{{ asset('images/web.png') }}" />
+@endsection
+
 @section('content')
 
                     <form method="POST" action="{{ route('polls.store') }}">
