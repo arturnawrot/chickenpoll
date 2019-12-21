1870488492620ae75a354a89311c7fbe62fc5b80
Arturek1 <artur.programista2@gmail.com>
2019-12-21 11:02:10 -0500

    String overflow in ProgressBar.vue fixed

--- resources/js/components/ProgressBar.vue
+++ resources/js/components/ProgressBar.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="progress-bar" role="progressbar" :aria-valuenow="percentage" aria-valuemin="0" aria-valuemax="100" :style="{'width': percentage + '%' }">
         <span :style="{'color': color}">
-            {{ votes }} votes
+            <div v-if="percentage > 8">{{votes}}<span v-if="votes > 1">votes</span><span v-else>vote</span>
+            </div>
+            <div v-else>{{ votes }}</div>
         </span>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['percentage', 'votes', 'color']
+        props: ['percentage', 'votes', 'color'],
     }
 </script>
 
 <style scoped>
 span {
-    margin-left: 20px;
+    margin-left: 5px;
 }
 </style>
