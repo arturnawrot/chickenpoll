21424f0e9144b55d6436d6965fc658d20900251d
Arturek1 <artur.programista2@gmail.com>
2019-12-21 11:12:05 -0500

    Whitespace added

--- resources/js/components/ProgressBar.vue
+++ resources/js/components/ProgressBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="progress-bar" role="progressbar" :aria-valuenow="percentage" aria-valuemin="0" aria-valuemax="100" :style="{'width': percentage + '%' }">
         <span :style="{'color': color}">
-            <div v-if="percentage > 8">{{votes}}<span v-if="votes > 1"> votes</span><span v-else>vote</span>
+            <div v-if="percentage > 8">{{votes}}<span v-if="votes > 1"> votes</span><span v-else> vote</span>
             </div>
             <div v-else>{{ votes }}</div>
         </span>
