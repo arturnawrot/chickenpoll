b59f6c5c48e9a4f8db5963b14ce2a1e1b3137abb
Arturek1 <artur.programista2@gmail.com>
2019-12-20 20:10:25 -0500

    LogVisitors middleware - error handling

--- app/Http/Middleware/LogVisitors.php
+++ app/Http/Middleware/LogVisitors.php
@@ -22,10 +22,10 @@ class LogVisitors
         $info = [
             'ip' => $ip,
             'referer' => $_SERVER["HTTP_REFERER"] ?? '0',
-            'user_agent' => $_SERVER['HTTP_USER_AGENT'],
-            'method' => $_SERVER['REQUEST_METHOD'],
-            'url' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
-            'language' => substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2),
+            'user_agent' => $_SERVER['HTTP_USER_AGENT'] ?? '0',
+            'method' => $_SERVER['REQUEST_METHOD'] ?? '0',
+            'url' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?? '0',
+            'language' => substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) ?? '0',
             'created_at' => now(),
             'agent' => new \Jenssegers\Agent\Agent()
         ];
