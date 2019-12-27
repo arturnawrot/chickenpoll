f949c7f274d63d6e6d0dfe5fff48d030f59c880a
Arturek1 <artur.programista2@gmail.com>
2019-12-26 19:44:29 -0500

    EasyPoll.com to ChickenPoll.com in the footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -5,7 +5,7 @@
         &nbsp;&nbsp;<span><a class="report-link" href="">Contact</a></span>
         </div>
         <div class="row justify-content-center">
-            <span>EasyPoll.com - All rights reserved 2019</span>
+            <span>ChickenPoll.com - All rights reserved 2019</span>
         </div>
     </div>
 </footer>
