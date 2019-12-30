c0d62275787b69040c62f0187fd694f9893f5a67
Arturek1 <artur.programista2@gmail.com>
2019-12-30 18:45:45 -0500

    Spatie/Socialite callback added

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
 
 class LoginController extends Controller
 {
