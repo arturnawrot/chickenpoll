4120964c76f6892b9a110d827dab2a6f1af6304d
Arturek1 <artur.programista2@gmail.com>
2020-01-03 16:56:01 +0000

    Ad banners changed

--- resources/views/inc/banners/2.blade.php
+++ resources/views/inc/banners/2.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center mb-5">
     <a href="{{ route('redirect.index') }}" target="_top">
-        <img class="img-fluid" src="{{ asset('images/mountains.jpg') }}" alt="Best VPN" title="Best VPN for privacy" />
+        <img class="img-fluid" src="{{ asset('images/flowers.jpg') }}" alt="Best VPN" title="Best VPN for privacy" />
     </a>
 </div>
 
--- resources/views/inc/banners/banner.blade.php
+++ resources/views/inc/banners/banner.blade.php
@@ -1,6 +1,6 @@
 <div class="text-center mb-5">
     <a href="{{ route('redirect.index') }}" target="_top">
-        <img class="img-fluid" src="{{ asset('images/flowers.png') }}" alt="Best VPN" title="Best VPN for privacy"/>
+        <img class="img-fluid" src="{{ asset('images/mountain.png') }}" alt="Best VPN" title="Best VPN for privacy"/>
     </a>
 </div>
 
